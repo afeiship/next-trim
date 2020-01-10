216634570f075187446adb2f4a8dd799f2aabb51
afeiship <1290657123@qq.com>
2020-01-10 15:01:02 +0800

    add: cache todo

--- dist/next-trim.js
+++ dist/next-trim.js
@@ -3,7 +3,7 @@
  * description: Trim space or customize string.
  * url: https://github.com/afeiship/next-trim
  * version: 1.0.0
- * date: 2020-01-10 15:00:27
+ * date: 2020-01-10 15:00:56
  * license: MIT
  */
 
@@ -14,6 +14,7 @@
   nx.trim = function(inStr, inChar) {
     if (!inStr) return inStr;
     if (inChar) {
+      // todo: maybe cache
       var reg1 = new RegExp('^' + inChar + '+', 'g');
       var reg2 = new RegExp(inChar + '+$', 'g');
       return inStr.replace(reg1, '').replace(reg2, '');
--- src/next-trim.js
+++ src/next-trim.js
@@ -5,6 +5,7 @@
   nx.trim = function(inStr, inChar) {
     if (!inStr) return inStr;
     if (inChar) {
+      // todo: maybe cache
       var reg1 = new RegExp('^' + inChar + '+', 'g');
       var reg2 = new RegExp(inChar + '+$', 'g');
       return inStr.replace(reg1, '').replace(reg2, '');
